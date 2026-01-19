65d8c4ab58cdacf1dc1353846af2f8d4d649e588
João_Accioly <jguilhermeaccioly@gmail.com>
2026-01-19 23:52:18 +0000

    adicionando o lint:check e o lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Me desculpa por tudo que eu fiz pra você, espero que tudo de certo para você e muito obrigado por acreditar em mim.</h1>
-};
+function Home() {
+  return (
+    <h1>
+      Me desculpa por tudo que eu fiz pra você, espero que tudo de certo para
+      você e muito obrigado por acreditar em mim.
+    </h1>
+  );
+}
 
 export default Home;
