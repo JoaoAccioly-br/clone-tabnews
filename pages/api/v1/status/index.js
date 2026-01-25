d18760403547b861a9e5421919c6d14925239d8c
João_Accioly <jguilhermeaccioly@gmail.com>
2026-01-25 22:41:23 +0000

    finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ chave: "os alunos do curso.dev são acima da média" });
+  const updatedAt = new Date().toISOString();
+  const resultVersion = await database.query("show server_version;");
+  const databaseVersion = resultVersion.rows[0].server_version;
+  const databaseMaxConnectionsResult = await database.query(
+    "show max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "select count(*)::int from pg_stat_activity where datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpennedConnectionValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        used_connections: databaseOpennedConnectionValue,
+      },
+    },
+  });
 }
 
 export default status;
